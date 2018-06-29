5779c9ddc9e07a2890387b1117798d37830e48ab
Rich Edmunds <xBLADEx@users.noreply.github.com>
2018-06-28 21:51:55 -0600

    Template Strings

---
+++ template-strings.js
@@ -0,0 +1,16 @@
+const weapons = ['Battle Axe', 'Short Sword', 'Dagger', 'Flail'];
+
+function buildArmory(gear) {
+  return `
+    <ul class="armory">
+      ${gear.map(item => `<li>${item}</li>`).join('')}
+    </ul>
+  `;
+}
+
+const armory = `
+  View our armory:
+  ${buildArmory(weapons)}
+`;
+
+document.body.innerHTML = armory;
