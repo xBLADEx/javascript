8c4e60d90cd234a44158bae8a7b3879c89bf1f2f
Rich Edmunds <rich@richedmunds.com>
2018-07-04 15:54:00 -0600

    Add Array.some() and Array.every() examples.

---
+++ array-some-and-every.js
@@ -0,0 +1,17 @@
+//--------------------------------------------------------------
+// DESTRUCTURING FUNCTIONS
+//
+// CodePen: https://codepen.io/xBLADEx/pen/oyRzqM
+//--------------------------------------------------------------
+
+const ages = [32, 30, 5, 3, 1];
+
+// Array.some()
+// Returns 'true' or 'false'. True if at least one item meets condition.
+const adult = ages.some(age => age >= 18);
+
+// Array.every()
+// Returns true if every item passes condition.
+const allAdults = ages.every(age => age >= 18);
+
+console.log(allAdults);
