7cedc47f356f2f67761a496af573f85046b1f66e
Rich Edmunds <rich@richedmunds.com>
2018-07-09 21:51:51 -0600

    Add promise example.

---
+++ promise.js
@@ -0,0 +1,12 @@
+//--------------------------------------------------------------
+// FETCH THEN CATCH
+//
+// CodePen: https://codepen.io/xBLADEx/pen/xJxxVJ
+//--------------------------------------------------------------
+
+const p = new Promise((resolve, reject) => {
+	resolve('Sweet');
+	reject(Error('Shoot'));
+});
+
+p.then(data => console.log(data)).catch(error => console.log(error));
